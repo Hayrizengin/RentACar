4115927a2fe3f8af7c3063d5687a91b38c390412
Hayri <Hayri@DESKTOP-HSD1K26>
2024-03-08 11:45:30 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
+        }
+
+        public void Insert(Car car)
+        {
+            if (car.Description.Length > 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araç veritabanına başarıyla eklendi");
+            }
+            else
+            {
+                Console.WriteLine("Araç bilgileri hatalı!");
+            }
+        }
+    }
+}
